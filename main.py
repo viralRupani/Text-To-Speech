706c9039fb5113f462fd33c32a9561d61883d135
viral rupani <viralrupani12017@gmail.com>
2022-07-26 13:11:43 +0530

    only text

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ filetypes = (
 
 
 def open_file():
-    with open(filedialog.askopenfilename( filetypes=[('pdf file', '*.pdf'), ('text files', '*.txt')])) as file:
-        language = 'en'
-        try:
-            tts = gTTS(text=str(file.readlines()), lang=language, slow=False).save("TTS.mp3")
-            messagebox.showinfo(title='Completed', message='Your audio file has been saved as TTS')
-        except:
-            print('hehe')
-            messagebox.showerror(title="undefined character", message='Please enter file that only contains text')
+    try:
+        with open(filedialog.askopenfilename(filetypes=[('text files', '*.txt')])) as file:
+            language = 'en'
+            try:
+                gTTS(text=str(file.readlines()), lang=language, slow=False).save("TTS.mp3")
+                messagebox.showinfo(title='Completed', message='Your audio file has been saved as TTS')
+            except:
+                messagebox.showerror(title="undefined character", message='Please enter file that only contains text')
 
+    except:
+        pass
 
 window = Window(height=400, width=600)
 
